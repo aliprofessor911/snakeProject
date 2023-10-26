5ba2471bb221d0cb3309fd928412fec70693655e
Ali Ataya <appleluvver8@gmail.com>
2023-10-26 16:26:35 -0700

    new background code

---
+++ java/GameBackgrounds.java
@@ -0,0 +1,36 @@
+package com.gamecodeschool.c17snake;
+
+public class GameBackgrounds {
+
+    private String texture;
+    private String fileDirectory;
+
+    //constructor
+    public GameBackgrounds(String texture, String fileDirectory) {
+        this.texture = texture;
+        this.fileDirectory = fileDirectory;
+    }
+
+
+    //draw background
+    public void draw() {
+
+
+
+
+    }
+
+    //update
+    public void update() {
+
+
+    }
+    //move
+    public void move() {
+
+
+    }
+
+
+
+}
